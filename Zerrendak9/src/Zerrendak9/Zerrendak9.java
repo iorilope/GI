81f3ed220821f118ffdfb2e4d56c3181e3fbea0f
1ag3.iorilope <1ag3.iorilope@Enara-p22.tolosaldealh.eus>
2023-11-02 10:53:24 +0100

    Igotzen

---
+++ Zerrendak9/src/Zerrendak9/Zerrendak9.java
@@ -0,0 +1,18 @@
+package Zerrendak9;
+
+public class Zerrendak9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int kontagailua = 0;
+
+
+		int[] zerrendaZenbaki = {1,2,3,2,5,6,7,8,9,10};
+
+		// (2) i, i++ izan behar da sekuntzia gorakorra egoteko
+
+
+	}
+
+}
